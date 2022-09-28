c56ed20c9c1cd654ce504461ef6bec760187aabd
pradeepfk <114599148+pradeepfk@users.noreply.github.com>
2022-09-28 20:57:49 +0530

    dictionary exercise
    searching meaning of the word within the given dictionary

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("searching meaning of the word within the given dictionary")
+print("\npython ex 1:")
+dict1={"mutable":"value can be chnaged","immutable":"value cannot be change" ,"phone":"electronic device","bike":"vehicle"
+       ,"shreya":"girl" , "pradeep":"boy"}
+print(dict1)
+print("enter a letter to serach its meaning=")
+print( "meaning of the word is :", dict1[input()] )
